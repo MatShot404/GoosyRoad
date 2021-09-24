57a2e0560f5060bd34d938c5024e0750f866135a
Mat <matshot03@gmail.com>
2021-09-24 14:14:45 +0200

    Ajout deplacements Oie
    L'oie se déplace en se téléportant d'une case à l'autre

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const cases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 var textpos = 1
-var playerpos = 0
+var playerpos = 1
 var playercase = "case" + playerpos
 var lastcase = playerpos - 1
 
@@ -10,6 +10,7 @@ function initialize() {
     cases.forEach(element => {
         ReactDOM.render(<h1 className="casecontent"> {element} </h1>, document.getElementById("case" + textpos));
         textpos += 1
+        ReactDOM.render(<img className="imgOie" src="/img/OIE_1.png" alt="oie" ></img>, document.getElementById("case1"));
     });
 }
 
@@ -25,9 +26,15 @@ function butevent() {
 
 
 function playermove() {
-    ReactDOM.render(<img class="imgOie" src="/img/OIE_1.png" alt="oie" ></img>, document.getElementById("case" + playerpos));
-    //ReactDOM.render(<h1 class="casecontent"> 1 </h1>, document.getElementById("case" + lastcase));
+    ReactDOM.render(<img className="imgOie" src="/img/OIE_1.png" alt="oie" ></img>, document.getElementById("case" + playerpos));
+    lastcase += 1
     console.log(lastcase);
+    ReactDOM.render(<h1 className="casecontent"> DONE ! </h1>, document.getElementById('case' + lastcase))
+    document.getElementById('case' + lastcase).style.backgroundColor = "Green";
+}
+
+function playermovetrans() {
+
 }
 
 function reset() {
@@ -44,7 +51,7 @@ function openquestion() {
         case 1:
             alert("Good Job ! :)")
             break;
-        
+
         default:
             alert("Wrong")
             break;

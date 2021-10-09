4702155136753149c19a0a513a4255dc2b079fe0
Mat <matshot03@gmail.com>
2021-10-09 12:34:47 +0200

    ajout premieres questions
    questions fonctionnelles mais réponses a revoir

--- js/script.js
+++ js/script.js
@@ -3,36 +3,60 @@ var textpos = 1;
 var playerpos = 1;
 var playercase = "case" + playerpos;
 var lastcase = playerpos - 1;
+var numquestion = "question" + playerpos;
 
 var modal = document.getElementById("myModal");
 
+var listeQuestions = {
+    "1question": "Quel est le mot de passe le plus sécurisé ?",
+    "1reponse1": "zEg _1$= - Hn",
+    "1reponse2": "123456789",
+    "1reponse3": "Iambornin1992",
+    "1reponse4": "azerty123",
+    "1reponse": "ans1",
+
+    "2question": "Un mot de passe pour être idéal doit contenir :",
+    "2reponse1": "Une majuscule au début suivi par 4 lettres",
+    "2reponse2": "Des chiffres un peu partout ",
+    "2reponse3": "Des chiffres, des lettres et des caractères spéciaux",
+    "2reponse4": "Uniquement des lettres",
+    "2reponse": "ans3",
+
+    "3question": "Quels sont les deux périodicité acceptable auxquelles vous devez effectué une sauvegarde de vos données pour être protégé ?",
+    "3reponse1": "Quotidienne",
+    "3reponse2": "Hebdomadaire",
+    "3reponse3": "Mensuelle",
+    "3reponse4": "Annuelle",
+    "3reponse": "ans2",
+}
+
+
 //Gestion import XML
-loadXMLDoc();
-function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
-        }
-    };
-    xmlhttp.open("GET", "../data/questions.xml", true);
-    xmlhttp.send();
-}
-function myFunction(xml) {
-    var x, i, xmlDoc, txt;
-    xmlDoc = xml.responseXML;
-    txt = "";
-    x = xmlDoc.getElementsByTagName("questionLabel"+playerpos);
-    for (i = 0; i < x.length; i++) {
-        txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-    document.getElementById("question").innerHTML = txt;
-}
+// loadXMLDoc();
+// function loadXMLDoc() {
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             myFunction(this);
+//         }
+//     };
+//     xmlhttp.open("GET", "../data/questions.xml", true);
+//     xmlhttp.send();
+// }
+// function myFunction(xml) {
+//     var x, i, xmlDoc, txt;
+//     xmlDoc = xml.responseXML;
+//     txt = "";
+//     x = xmlDoc.getElementsByTagName("questionLabel" + playerpos);
+//     for (i = 0; i < x.length; i++) {
+//         txt += x[i].childNodes[0].nodeValue + "<br>";
+//     }
+//     document.getElementById("question").innerHTML = txt;
+// }
 
-//Envoi de la réponse lorsque l'utilisateur submit sa réponse
-document.getElementById('questionAnswer').addEventListener('submit', submitForm);
-
+//Initialise et Envoi de la réponse lorsque l'utilisateur submit sa réponse
 initialize();
+document.getElementById('questionAnswer').addEventListener('submit', submitForm);
 
 //Initialisation du jeu
 function initialize() {
@@ -51,6 +75,61 @@ function initialize() {
 
 //Ouverture de la modal de question 
 function butevent() {
+    switch (playerpos) {
+        case 1:
+            ReactDOM.render(
+                <h2 id="question">{listeQuestions["1question"]}</h2>, document.getElementById("question")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans1" id="ans1Label">{listeQuestions["1reponse1"]}</label>, document.getElementById("ans1Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans2" id="ans2Label">{listeQuestions["1reponse2"]}</label>, document.getElementById("ans2Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans3" id="ans3Label">{listeQuestions["1reponse3"]}</label>, document.getElementById("ans3Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans4" id="ans4Label">{listeQuestions["1reponse4"]}</label>, document.getElementById("ans4Label")
+            )
+            break;
+        case 2:
+            ReactDOM.render(
+                <h2 id="question">{listeQuestions["2question"]}</h2>, document.getElementById("question")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans1" id="ans1Label">{listeQuestions["2reponse1"]}</label>, document.getElementById("ans1Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans2" id="ans2Label">{listeQuestions["2reponse2"]}</label>, document.getElementById("ans2Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans3" id="ans3Label">{listeQuestions["2reponse3"]}</label>, document.getElementById("ans3Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans4" id="ans4Label">{listeQuestions["2reponse4"]}</label>, document.getElementById("ans4Label")
+            );
+            break;
+        case 3:
+            ReactDOM.render(
+                <h2 id="question">{listeQuestions["3question"]}</h2>, document.getElementById("question")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans1" id="ans1Label">{listeQuestions["3reponse1"]}</label>, document.getElementById("ans1Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans2" id="ans2Label">{listeQuestions["3reponse2"]}</label>, document.getElementById("ans2Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans3" id="ans3Label">{listeQuestions["3reponse3"]}</label>, document.getElementById("ans3Label")
+            );
+            ReactDOM.render(
+                <label htmlFor="ans4" id="ans4Label">{listeQuestions["3reponse4"]}</label>, document.getElementById("ans4Label")
+            );
+            break;
+        default:
+            break;
+    }
     modal.style.display = "block";
     //openquestion();
 }
@@ -58,18 +137,18 @@ function butevent() {
 //Affichage question/réponse
 var reponse = ['ans1', 'ans4', 'ans2', 'ans1', 'ans3', 'ans2', 'ans4', 'ans1', 'ans1', 'ans4', 'ans3', 'ans2'];
 var question = ['Quel est le mot de passe le plus sécurisé ?']
-ReactDOM.render(
-    <div>
-        <h2 id="question">{question[playerpos - 1]}</h2>
-    </div>,
-    document.getElementById("question")
-);
+// ReactDOM.render(
+//     <div>
+//         <h2 id="question">{question[playerpos - 1]}</h2>
+//     </div>,
+//     document.getElementById("question")
+// );
 
 //Gestion réponse envoyée
 function submitForm(e) {
     console.log(reponse);
     e.preventDefault();
-    if (document.getElementById(reponse[playerpos - 1]).checked == true) {
+    if (document.getElementById(reponse).checked == true) {
         console.log("Bonne réponse :)");
         playerpos += 1;
         playermovetrue();

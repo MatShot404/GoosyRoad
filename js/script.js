9cf3a57a4c2a6e5315d179424a5874b502ae4b71
Mathis <matshot03@gmail.com>
2021-10-17 16:41:46 +0200

    Ajout vrai JSON interne

--- js/script.js
+++ js/script.js
@@ -5,91 +5,107 @@ var playercase = "case" + playerpos;
 var lastcase = playerpos - 1;
 var lives = 2;
 
-var listeQuestions = {
-    "1question": "Quel est le mot de passe le plus sécurisé ?",
-    "1reponse1": "zEg_1$=-Hn",
-    "1reponse2": "123456789",
-    "1reponse3": "Iwasbornin1992",
-    "1reponse4": "azerty123",
-    "1reponse": "ans1",
+var listeQuestions = [
+    {
+        "question": "Quel est le mot de passe le plus sécurisé ?",
+        "reponse1": "zEg_1$=-Hn",
+        "reponse2": "123456789",
+        "reponse3": "Iwasbornin1992",
+        "reponse4": "azerty123",
+        "reponse": "ans1"
+    },
+    {
+        "question": "Un mot de passe pour être idéal doit contenir :",
+        "reponse1": "Une majuscule au début suivi par 4 lettres",
+        "reponse2": "Des chiffres un peu partout ",
+        "reponse3": "Des chiffres, des lettres et des caractères spéciaux",
+        "reponse4": "Uniquement des lettres",
+        "reponse": "ans3"
+    },
+    {
+        "question": "A quelle périodicité maximale acceptable devez-vous effectuer une sauvegarde de vos données pour être protégé ?",
+        "reponse1": "Quotidienne",
+        "reponse2": "Hebdomadaire",
+        "reponse3": "Mensuelle",
+        "reponse4": "Annuelle",
+        "reponse": "ans2"
+    },
 
-    "2question": "Un mot de passe pour être idéal doit contenir :",
-    "2reponse1": "Une majuscule au début suivi par 4 lettres",
-    "2reponse2": "Des chiffres un peu partout ",
-    "2reponse3": "Des chiffres, des lettres et des caractères spéciaux",
-    "2reponse4": "Uniquement des lettres",
-    "2reponse": "ans3",
+    {
+        "question": "Quels acronyme ci-dessous n'est pas en lien avec le WiFi ?",
+        "reponse1": "WPA",
+        "reponse2": "WEP",
+        "reponse3": "GTR",
+        "reponse4": "IEEE",
+        "reponse": "ans3"
+    },
+    {
+        "question": "Pour supprimer ses données personnelles qui peut-on contacter ?",
+        "reponse1": "La Commission Nationale de l'Informatique et des Libertés (CNIL)",
+        "reponse2": "La Commission Nationale de Protection à l'Image (CNPI)",
+        "reponse3": "La comission Européenne",
+        "reponse4": "Commission chargée de l'application de l'article 26 de la Constitution",
+        "reponse": "ans1"
+    },
+    {
+        "question": "Qui sont les chapeaux blancs (White Hat) ?",
+        "reponse1": "Un groupe de hacker russe ayant comme image de marque les chapeaux blancs",
+        "reponse2": "Un ensemble de membres d'une entreprise ayant pour but de protéger les utilisateurs d'un service",
+        "reponse3": "Des humoristes pas très marrants",
+        "reponse4": "Des hackers de bonne foie (étiques) embauchés par des entreprises ayant pour objectif de découvrir des failles en et les en prévenir",
+        "reponse": "ans4"
+    },
+    {
+        "question": "Est-ce conseiller d'utiliser plusieurs fois un même mot de passe ?",
+        "reponse1": "Oui",
+        "reponse2": "Non",
+        "reponse3": "",
+        "reponse4": "",
+        "reponse": "ans2"
+    },
 
-    "3question": "A quelle périodicité maximale acceptable devez-vous effectuer une sauvegarde de vos données pour être protégé ?",
-    "3reponse1": "Quotidienne",
-    "3reponse2": "Hebdomadaire",
-    "3reponse3": "Mensuelle",
-    "3reponse4": "Annuelle",
-    "3reponse": "ans2",
+    {
+        "question": "A quoi servent les mises à jours de vos applications ?",
+        "reponse1": "A ajouter des bugs dans l'application",
+        "reponse2": "A réparé les failles de sécurité",
+        "reponse3": "A rien, le logiciel est déjà bien comme il est",
+        "reponse4": "A aller chez les concurrents",
+        "reponse": "ans2"
+    },
 
-    "4question": "Quels acronyme ci-dessous n'est pas en lien avec le WiFi ?",
-    "4reponse1": "WPA",
-    "4reponse2": "WEP",
-    "4reponse3": "GTR",
-    "4reponse4": "IEEE",
-    "4reponse": "ans3",
-
-    "5question": "Pour supprimer ses données personnelles qui peut-on contacter ?",
-    "5reponse1": "La Commission Nationale de l'Informatique et des Libertés (CNIL)",
-    "5reponse2": "La Commission Nationale de Protection à l'Image (CNPI)",
-    "5reponse3": "La comission Européenne",
-    "5reponse4": "Commission chargée de l'application de l'article 26 de la Constitution",
-    "5reponse": "ans1",
-
-    "6question": "Qui sont les chapeaux blancs (White Hat) ?",
-    "6reponse1": "Un groupe de hacker russe ayant comme image de marque les chapeaux blancs",
-    "6reponse2": "Un ensemble de membres d'une entreprise ayant pour but de protéger les utilisateurs d'un service",
-    "6reponse3": "Des humoristes pas très marrants",
-    "6reponse4": "Des hackers de bonne foie (étiques) embauchés par des entreprises ayant pour objectif de découvrir des failles en et les en prévenir",
-    "6reponse": "ans4",
-
-    "7question": "Est-ce conseiller d'utiliser plusieurs fois un même mot de passe ?",
-    "7reponse1": "Oui",
-    "7reponse2": "Non",
-    "7reponse3": "",
-    "7reponse4": "",
-    "7reponse": "ans2",
-
-    "8question": "A quoi servent les mises à jours de vos applications ?",
-    "8reponse1": "A ajouter des bugs dans l'application",
-    "8reponse2": "A réparé les failles de sécurité",
-    "8reponse3": "A rien, le logiciel est déjà bien comme il est",
-    "8reponse4": "A aller chez les concurrents",
-    "8reponse": "ans2",
-
-    "9question": "Avec quoi sécurisé l'accès à vos appareils si quelqu'un vous les volés ?",
-    "9reponse1": "Un code à 4 chiffres",
-    "9reponse2": "Un cadena (physique)",
-    "9reponse3": "Une coque résistante",
-    "9reponse4": "Un schéma",
-    "9reponse": "ans4",
-
-    "10question": "Pour veiller à la sécurité de votre machine et de vos données pour un téléchargement de logiciel il ne faut pas :",
-    "10reponse1": "Téléchargez vos programmes sur les sites de leurs éditeurs",
-    "10reponse2": "Décocher toutes les cases proposant d’installer des logiciels complémentaires",
-    "10reponse3": "Cracker l'application pour l'avoir gratuitement si payante",
-    "10reponse4": "Désactivez l’ouverture automatique des documents téléchargés et lancez une analyse antivirus",
-    "10reponse": "ans3",
-
-    "11question": "Dans les 4 propositions suivantes, laquelle est déconseillé pour vérifier si un mail est sécurisé ?",
-    "11reponse1": "Ouvrir les pièces jointes et donc vérifier les documents envoyés",
-    "11reponse2": "Vérifier l'identité de l'envoyeur (son nom, mail) ",
-    "11reponse3": "Se renseigner sur internet par rapport au mail reçu",
-    "11reponse4": "Inspecter le mail pour trouver de possible fautes d'orthographe",
-    "11reponse": "ans1",
-
-    "12question": "Dans les réponses suivantes, laquelle ne met pas en jeu les informations sensibles d'une entreprise ?",
-    "12reponse1": "Brancher un support amovible personnel sur l'ordinateur",
-    "12reponse2": "Enregistrer les informations professionnelle sur des équipements personnels",
-    "12reponse3": "Envoyer des mails avec un compte utilisé à des fins personnel",
-    "12reponse4": "Éteindre / verrouiller un ordinateur professionnel quand il n'est pas utilisé",
-    "12reponse": "ans4",
-}
+    {
+        "question": "Avec quoi sécurisé l'accès à vos appareils si quelqu'un vous les volés ?",
+        "reponse1": "Un code à 4 chiffres",
+        "reponse2": "Un cadena (physique)",
+        "reponse3": "Une coque résistante",
+        "reponse4": "Un schéma",
+        "reponse": "ans4"
+    },
+    {
+        "question": "Pour veiller à la sécurité de votre machine et de vos données pour un téléchargement de logiciel il ne faut pas :",
+        "reponse1": "Téléchargez vos programmes sur les sites de leurs éditeurs",
+        "reponse2": "Décocher toutes les cases proposant d’installer des logiciels complémentaires",
+        "reponse3": "Cracker l'application pour l'avoir gratuitement si payante",
+        "reponse4": "Désactivez l’ouverture automatique des documents téléchargés et lancez une analyse antivirus",
+        "reponse": "ans3"
+    },
+    {
+        "question": "Dans les 4 propositions suivantes, laquelle est déconseillé pour vérifier si un mail est sécurisé ?",
+        "reponse1": "Ouvrir les pièces jointes et donc vérifier les documents envoyés",
+        "reponse2": "Vérifier l'identité de l'envoyeur (son nom, mail) ",
+        "reponse3": "Se renseigner sur internet par rapport au mail reçu",
+        "reponse4": "Inspecter le mail pour trouver de possible fautes d'orthographe",
+        "reponse": "ans1"
+    },
+    {
+        "question": "Dans les réponses suivantes, laquelle ne met pas en jeu les informations sensibles d'une entreprise ?",
+        "reponse1": "Brancher un support amovible personnel sur l'ordinateur",
+        "reponse2": "Enregistrer les informations professionnelle sur des équipements personnels",
+        "reponse3": "Envoyer des mails avec un compte utilisé à des fins personnel",
+        "reponse4": "Éteindre / verrouiller un ordinateur professionnel quand il n'est pas utilisé",
+        "reponse": "ans4"
+    }
+]
 
 //var listeQuestions = require('../data/listeQuestions.json');
 
@@ -114,20 +130,23 @@ function initialize() {
 
 //Ouverture de la modal de question avec contenu en fonction de la position du joueur
 function butevent() {
+    // ReactDOM.render(
+    //     <h2 id="question">{listeQuestions[playerpos + "question"]}</h2>, document.getElementById("question")
+    // );
     ReactDOM.render(
-        <h2 id="question">{listeQuestions[playerpos + "question"]}</h2>, document.getElementById("question")
+        <h2 id="question">{listeQuestions[playerpos - 1].question}</h2>, document.getElementById("question")
     );
     ReactDOM.render(
-        <label htmlFor="ans1" id="ans1Label">{listeQuestions[playerpos + "reponse1"]}</label>, document.getElementById("ans1Label")
+        <label htmlFor="ans1" id="ans1Label">{listeQuestions[playerpos - 1].reponse1}</label>, document.getElementById("ans1Label")
     );
     ReactDOM.render(
-        <label htmlFor="ans2" id="ans2Label">{listeQuestions[playerpos + "reponse2"]}</label>, document.getElementById("ans2Label")
+        <label htmlFor="ans2" id="ans2Label">{listeQuestions[playerpos - 1].reponse2}</label>, document.getElementById("ans2Label")
     );
     ReactDOM.render(
-        <label htmlFor="ans3" id="ans3Label">{listeQuestions[playerpos + "reponse3"]}</label>, document.getElementById("ans3Label")
+        <label htmlFor="ans3" id="ans3Label">{listeQuestions[playerpos - 1].reponse3}</label>, document.getElementById("ans3Label")
     );
     ReactDOM.render(
-        <label htmlFor="ans4" id="ans4Label">{listeQuestions[playerpos + "reponse4"]}</label>, document.getElementById("ans4Label")
+        <label htmlFor="ans4" id="ans4Label">{listeQuestions[playerpos - 1].reponse4}</label>, document.getElementById("ans4Label")
     )
     questionModal.style.display = "block";
     //openquestion();
@@ -138,7 +157,7 @@ function submitForm(e) {
     e.preventDefault();
 
     if (playerpos == 12) {
-        if (document.getElementById(listeQuestions[playerpos + "reponse"]).checked == true) {
+        if (document.getElementById(listeQuestions[playerpos - 1].reponse).checked == true) {
             checkLives();
             ReactDOM.render(
                 <h1 className="casecontent"> DONE ! </h1>, document.getElementById("case" + playerpos));
@@ -153,7 +172,7 @@ function submitForm(e) {
         questionModal.style.display = "none";
 
     }
-    if (document.getElementById(listeQuestions[playerpos + "reponse"]).checked == true) {
+    if (document.getElementById(listeQuestions[playerpos - 1].reponse).checked == true) {
         console.log("Bonne réponse :)");
         playerpos += 1;
         playermovetrue();
@@ -176,8 +195,8 @@ function playermovetrue() {
     lastcase += 1;
 
     ReactDOM.render(
-        <h1 className="casecontent"> DONE ! </h1>, document.getElementById("case" + lastcase));
-    document.getElementById("case" + lastcase).style.backgroundColor = "Green";
+        <svg class="w-6 h-6 casecontent" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>, document.getElementById("case" + lastcase));
+    document.getElementById("case" + lastcase).style.backgroundColor = "rgba(0, 128, 0, 0.6)";
 }
 
 // Réponse fausse :
@@ -188,8 +207,8 @@ function playermovefalse() {
     lives -= 1;
 
     ReactDOM.render(
-        <h1 className="casecontent"> Wrong ! </h1>, document.getElementById("case" + lastcase));
-    document.getElementById("case" + lastcase).style.backgroundColor = "Red";
+        <svg class="w-6 h-6 casecontent" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>, document.getElementById("case" + lastcase));
+    document.getElementById("case" + lastcase).style.backgroundColor = "rgba(128, 0, 0, 0.6)";
 }
 
 function checkLives() {
@@ -228,8 +247,6 @@ function gameOver() {
 function playermovetransition() {
     // Move
 }
-
-
 
 //@TODO
 function settings() {
